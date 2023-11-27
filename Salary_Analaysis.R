4dda2621bfc49520631b5b30f33b3bff4f8a2dcf
sandun7787 <sandunwarnasooreya345@gmail.com>
2023-11-27 15:04:32 +0530

    new test case

--- Salary_Analaysis.R
+++ Salary_Analaysis.R
@@ -1,1 +1,17 @@
 # Normal Distribution Test
+
+#significant level:0.05
+#confident level:0.95
+
+attach(Salaries)
+
+library(nortest)
+
+
+#Anderson Darling Normality Test
+
+ad.test(salary)
+
+#Lilefore.Test Normality Test
+
+lillie.test(salary)
